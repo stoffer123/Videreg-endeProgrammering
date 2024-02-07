692f7c6f45f14b3561c133a9270226fa1e727f1e
stoffer123 <christophermikkelsen@live.dk>
2024-02-07 21:28:53 +0100

    SystemTextIOImpl added

---
+++ FirstSteps/FirstSteps/src/main/java/dk/cphbusiness/vp/f2024/textio/SystemTextIOImpl.java
@@ -0,0 +1,28 @@
+package dk.cphbusiness.vp.f2024.textio;
+
+import java.util.Scanner;
+
+public class SystemTextIOImpl implements TextIO
+{
+    private static Scanner scanner = new Scanner(System.in);
+    @Override
+    public void put(String str)
+    {
+        System.out.println(str);
+    }
+
+    @Override
+    public void clear()
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            System.out.println();
+        }
+    }
+
+    @Override
+    public String get()
+    {
+        return scanner.nextLine();
+    }
+}
