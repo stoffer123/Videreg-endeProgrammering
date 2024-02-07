1a64272e2755ca36c1b1be7042db8cddfc460831
stoffer123 <christophermikkelsen@live.dk>
2024-02-07 17:22:50 +0100

    inial lamp commit

--- FirstSteps/FirstSteps/src/main/java/dk/cphbusiness/vp/f2024/Main.java
+++ FirstSteps/FirstSteps/src/main/java/dk/cphbusiness/vp/f2024/Main.java
@@ -6,17 +6,14 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //test carfactory
+        //test carFactory
 
-        Factory factory = new Factory();
-        factory.mainMenu();
-
-
-
-
-
+         Factory factory = new Factory();
+         factory.mainMenu();
 
     }
+
+
 
 
 
---
+++ FirstSteps/FirstSteps/src/main/java/dk/cphbusiness/vp/f2024/carfactory/lamp/Lamp.java
@@ -0,0 +1,19 @@
+package dk.cphbusiness.vp.f2024.carfactory.lamp;
+
+public class Lamp {
+    private boolean isOn;
+    private int lightLevel;
+    private String name;
+    private static int lampNumber;
+
+    static
+    {
+        lampNumber = 1;
+    }
+
+    public Lamp()
+    {
+
+    }
+
+}
