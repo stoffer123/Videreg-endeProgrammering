899a33e0e4833cdd63ba3f2018f5fa76d7955eed
stoffer123 <christophermikkelsen@live.dk>
2024-02-07 21:49:31 +0100

    SystemTextIOImpl added

--- FirstSteps/FirstSteps/src/main/java/dk/cphbusiness/vp/f2024/textio/SystemTextIOImpl.java
+++ FirstSteps/FirstSteps/src/main/java/dk/cphbusiness/vp/f2024/textio/SystemTextIOImpl.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class SystemTextIOImpl implements TextIO
 {
     private static Scanner scanner = new Scanner(System.in);
+
     @Override
     public void put(String str)
     {
@@ -14,15 +15,17 @@ public class SystemTextIOImpl implements TextIO
     @Override
     public void clear()
     {
-        for(int i = 0; i < 100; i++)
+        for (int i = 0; i < 100; i++)
         {
             System.out.println();
         }
     }
 
-    @Override
+   @Override
     public String get()
     {
         return scanner.nextLine();
     }
+
+
 }
---
+++ FirstSteps/FirstSteps/src/main/java/dk/cphbusiness/vp/f2024/textio/TextIODemo.java
@@ -0,0 +1,9 @@
+package dk.cphbusiness.vp.f2024.textio;
+
+public class TextIODemo
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
